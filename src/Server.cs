ebb2511057e8d2f3a935577fd87f70d7290db765
oebelus <imaneelfidrissi@hotmail.com>
2024-07-14 04:38:39 +0100

    first

--- src/Server.cs
+++ src/Server.cs
@@ -7,3 +7,5 @@ Console.WriteLine("Logs from your program will appear here!");
 TcpListener server = new(IPAddress.Any, 4221);
 server.Start();
 server.AcceptSocket(); // wait for client
+
+Console.WriteLine("HTTP/1.1 200 OK\r\n\r\n");
